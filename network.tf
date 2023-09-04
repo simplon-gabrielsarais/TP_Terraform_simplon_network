0379ef2bea11afe049fdafe95dc043ce8b36bf69
Simplon-gabrielsarais <99554412+simplon-gabrielsarais@users.noreply.github.com>
2023-09-04 11:38:25 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,13 +2,13 @@
 
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
-  location = "East US"
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "example" {
   name                = var.vnet_name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
