a7263f2420a79b506b704bae9f267217eba602c7
Simplon-gabrielsarais <99554412+simplon-gabrielsarais@users.noreply.github.com>
2023-09-04 11:27:10 +0200

    Create network.tf
    add network.tf

---
+++ network.tf
@@ -0,0 +1,40 @@
+# network.tf
+
+variable "resource_group_name" {
+  description = "Nom du groupe de ressources Azure"
+}
+
+variable "vnet_name" {
+  description = "Nom du réseau virtuel Azure"
+}
+
+variable "subnet_name" {
+  description = "Nom du sous-réseau Azure"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+output "network_id" {
+  value = azurerm_virtual_network.example.id
+}
+
+output "subnet_id" {
+  value = azurerm_subnet.example.id
+}
